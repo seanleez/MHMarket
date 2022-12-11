bb4fa2dd3e87dfb6a5250ee9c3919f094a4436aa
Van Vien Dong <vanviendong96@gmail.com>
2022-12-11 19:02:33 +0700

    add modal

--- src/modules/StallApplicationForm/pages/Payment.tsx
+++ src/modules/StallApplicationForm/pages/Payment.tsx
@@ -1,19 +1,31 @@
+import SuccessDialog from '@components/common/dialog/SuccessDialog';
 import CustomField from '@components/common/lease-and-application/CustomField';
 import { Box, Button, MenuItem, TextField, Typography } from '@mui/material';
-import React, { useEffect, useMemo, useState } from 'react';
+import { useSnackbar } from 'notistack';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { IStallFormShared } from '.';
 import { PAYMENT_METHODS } from '../../../const/const';
 import { TPair } from '../../../const/interface';
+import applicationApis from '../../../services/applicationsApis';
 import ImageUploader from '../components/ImageUploader';
 import FormContainer from '../layouts';
 import { useStallData } from './EditStallApplication';
+import PaymentModal from './PaymentModal';
 
 const Payment = (props: IStallFormShared) => {
-  const { applicationInfor } = useStallData();
+  const { applicationInfor, commonData } = useStallData();
+  const [openSuccessDialog, setOpenSuccessDialog] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(false);
   const [paymentMethod, setPaymentMethod] = useState<number>(
     applicationInfor.payment_method || PAYMENT_METHODS[0].value
   );
 
+  const isApproved = useRef<boolean | undefined>();
+
+  const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
   const labelValuePair: TPair[] = useMemo(() => {
     return [
       {
@@ -46,85 +58,161 @@ const Payment = (props: IStallFormShared) => {
     ];
   }, [applicationInfor]);
 
-  return (
-    <FormContainer {...props} shouldGray={false} step={2}>
-      <Box
-        sx={{
-          margin: '20px 0',
-          width: '100%',
-        }}>
-        <h3>ORDER OF PAYMENT</h3>
-        <Box sx={{ width: '60%', marginBottom: '50px' }}>
-          {labelValuePair.map((pair: TPair, i: number) => (
-            <CustomField key={i} pair={pair} />
-          ))}
-        </Box>
-        <Typography>
-          Once you submit the application form, you will be redirected to the
-          Quezon City’s e-Payment Page. If your payment is successful, your
-          application will be submitted and the status will be updated to For
-          Payment Verification. If your payment is unsuccessful (eg: cancelled,
-          declined, etc.), your application will be saved but the status will
-          remain to be In Progress. You have the following option:
-        </Typography>
-        <Typography sx={{ textIndent: '50px' }}>
-          1. Click the Submit button again and try paying online.
-        </Typography>
-        <Typography sx={{ textIndent: '50px' }}>
-          2. Update your mode of payment to Manual. You may pay to the market
-          collector or you may go to the City Treasurer’s Office to pay for your
-          fees.
-        </Typography>
+  const handleSubmit = () => {
+    if (!id) return;
+    (async () => {
+      try {
+        await applicationApis.updateAppPayment(id, commonData);
+        setOpenSuccessDialog(true);
+      } catch (error) {
+        enqueueSnackbar(error as string);
+      }
+    })();
+  };
 
+  const handleSubmitModal = (comment: string) => {
+    const payload = {
+      application_id: id,
+      comment: comment,
+      is_approved: isApproved.current,
+    };
+    console.log(payload);
+    (async () => {
+      try {
+        await applicationApis.confirmAppPayment(id, payload);
+        setOpenSuccessDialog(true);
+        navigate('/application-list');
+      } catch (error) {
+        enqueueSnackbar(error as string);
+      }
+    })();
+  };
+
+  return (
+    <>
+      <FormContainer {...props} shouldGray={false} step={2}>
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '10px',
             margin: '20px 0',
+            width: '100%',
           }}>
-          <h4>Payment Method</h4>
-          <TextField
-            select
-            size="small"
-            value={paymentMethod}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPaymentMethod(Number(e.target.value))
-            }>
-            {PAYMENT_METHODS.map((method: any) => (
-              <MenuItem key={method.value} value={method.value}>
-                {method.label}
-              </MenuItem>
+          <h3>ORDER OF PAYMENT</h3>
+          <Box sx={{ width: '60%', marginBottom: '50px' }}>
+            {labelValuePair.map((pair: TPair, i: number) => (
+              <CustomField key={i} pair={pair} />
             ))}
-          </TextField>
-        </Box>
-        <Button variant="contained" size="small">
-          Submit
-        </Button>
-        {paymentMethod === 2 && (
-          <Box sx={{ margin: '20px 0' }}>
-            <Typography sx={{ marginBottom: '10px' }}>
-              Proof of Transfer
-            </Typography>
-            <ImageUploader />
           </Box>
-        )}
-        <Box
-          sx={{
-            margin: '10px 0',
-            display: 'flex',
-            justifyContent: 'center',
-            gap: '10px',
-          }}>
-          <Button size="small" variant="outlined" onClick={props.handleBack}>
-            Back
+          <Typography>
+            Once you submit the application form, you will be redirected to the
+            Quezon City’s e-Payment Page. If your payment is successful, your
+            application will be submitted and the status will be updated to For
+            Payment Verification. If your payment is unsuccessful (eg:
+            cancelled, declined, etc.), your application will be saved but the
+            status will remain to be In Progress. You have the following option:
+          </Typography>
+          <Typography sx={{ textIndent: '50px' }}>
+            1. Click the Submit button again and try paying online.
+          </Typography>
+          <Typography sx={{ textIndent: '50px' }}>
+            2. Update your mode of payment to Manual. You may pay to the market
+            collector or you may go to the City Treasurer’s Office to pay for
+            your fees.
+          </Typography>
+
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: '10px',
+              margin: '20px 0',
+            }}>
+            <h4>Payment Method</h4>
+            <TextField
+              select
+              size="small"
+              disabled={commonData?.status === 3}
+              value={paymentMethod}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPaymentMethod(Number(e.target.value))
+              }>
+              {PAYMENT_METHODS.map((method: any) => (
+                <MenuItem key={method.value} value={method.value}>
+                  {method.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Box>
+          <Button
+            variant="contained"
+            size="small"
+            disabled={commonData?.status === 3}
+            onClick={handleSubmit}>
+            Submit
           </Button>
-          <Button size="small" variant="outlined">
-            Close
-          </Button>
+          {paymentMethod === 2 && (
+            <Box sx={{ margin: '20px 0' }}>
+              <Typography sx={{ marginBottom: '10px' }}>
+                Proof of Transfer
+              </Typography>
+              <ImageUploader max={1} name="proof_of_transfer" />
+            </Box>
+          )}
+          <Box
+            sx={{
+              margin: '10px 0',
+              display: 'flex',
+              justifyContent: 'center',
+              gap: '10px',
+            }}>
+            <Button size="small" variant="outlined" onClick={props.handleBack}>
+              Back
+            </Button>
+            {commonData?.status === 0 ? (
+              <>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="success"
+                  onClick={() => {
+                    isApproved.current = true;
+                    setOpenModal(true);
+                  }}>
+                  Accept
+                </Button>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="error"
+                  onClick={() => {
+                    isApproved.current = false;
+                    setOpenModal(true);
+                  }}>
+                  Reject
+                </Button>
+              </>
+            ) : (
+              <></>
+            )}
+            <Button size="small" variant="outlined">
+              Close
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </FormContainer>
+      </FormContainer>
+      <SuccessDialog
+        message="Submit Successfully"
+        openProp={openSuccessDialog}
+        onCloseDialog={() => {
+          setOpenSuccessDialog(false);
+          navigate('/application-list');
+        }}
+      />
+      <PaymentModal
+        openModal={openModal}
+        onCloseModal={() => setOpenModal(false)}
+        onSubmitModal={handleSubmitModal}
+      />
+    </>
   );
 };
 
--- src/services/applicationsApis.ts
+++ src/services/applicationsApis.ts
@@ -6,7 +6,11 @@ const applicationApis = {
   getApplication: (id: TId) => axiosClient.get(`/applications/${id}`),
   deleteApplication: (id: TId) => axiosClient.delete(`/applications/${id}`),
   submitApplication: (data: any, isDraft = false) => axiosClient.post(`/applications`, {...data, draft: isDraft}),
-  updateApplication: (data: any, isDraft = false) => axiosClient.put(`/applications/${data.application_id}/docs`, {...data, draft: isDraft})
+  updateApplication: (data: any, isDraft = false) => axiosClient.put(`/applications/${data.application_id}/docs`, {...data, draft: isDraft}),
+  updateAppPayment: (id: TId, data: any) =>
+    axiosClient.put(`/applications/${id}/payment`, data),
+  confirmAppPayment: (id: TId, payload: any) =>
+    axiosClient.put(`/applications/${id}/confirm`, payload),
 };
 
 export default applicationApis;
